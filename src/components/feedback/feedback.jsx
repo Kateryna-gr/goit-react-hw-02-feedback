e71701db4111a02be831f78cd45e6ee15272aef2
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-10-25 00:47:37 +0300

    task 1

---
+++ src/components/feedback/feedback.jsx
@@ -0,0 +1,57 @@
+import { FeedbackOptions } from 'components/feedback-options/feedback-options';
+import { Section } from 'components/section/section';
+import { Statistics } from 'components/statistics/statistics';
+import React, { Component } from 'react';
+import { Container } from './feedback.styled';
+
+export class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  constructor() {
+    super();
+    this.handleCounter = this.handleCounter.bind(this);
+  }
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    if (this.state.good) {
+      return Math.round((this.state.good * 100) / this.countTotalFeedback());
+    }
+  };
+
+  handleCounter = evt => {
+    let prevState = this.state;
+    let key = evt.target.name;
+
+    this.setState({
+      [key]: prevState[key] + 1,
+    });
+  };
+
+  render() {
+    const state = this.state;
+
+    return (
+      <Container>
+        <Section title="Please leave feedback" />
+        <FeedbackOptions options={state} onLeaveFeedback={this.handleCounter} />
+
+        <Section title="Statistics" />
+        <Statistics
+          good={state.good}
+          neutral={state.neutral}
+          bad={state.bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </Container>
+    );
+  }
+}
